3877043739b63e9b6504f59d1b69aa32ce8833f2
Deri <116681280+DerickMuniz@users.noreply.github.com>
2026-05-24 18:20:46 -0300

    Refactor main function and add benchmarks
    Refactor main function for benchmark selection and execution. Added two benchmarks: simulate_streaming and simulate_matrix_conv. In addition to improving data traffic.

--- software/benchmark_c/main.c
+++ software/benchmark_c/main.c
@@ -8,6 +8,9 @@
 #include "cache.h"
 #include "config.h"
 
+// configs para banchmarks
+#define L2_SIZE_BYTES (128 * 1024)
+#define ARRAY_SIZE    (L2_SIZE_BYTES * 2 / 4)
 
 typedef struct {
     double hit_rate;
@@ -24,9 +27,13 @@ typedef struct {
 static void print_benchmark_table_flexible(const char *benchmark_name, const char *config_label, PolicyResult *results, int count) {
     if (count < 1) return;
 
-    int col_width = 14;
+    //int col_width = 14;
 
     printf("\n");
+    printf("-----------------------------------------------------------------------------------------\n");
+    printf("                                  %s\n", benchmark_name);
+    printf("                         (%s)\n", config_label);
+    printf("-----------------------------------------------------------------------------------------\n");
 
     // cabeçalho
     printf("%-22s| %-14s", "Métrica", results[0].label);
@@ -75,12 +82,67 @@ static void print_benchmark_table_flexible(const char *benchmark_name, const cha
                 lat, (lat == 1 ? " " : "s"), 
                 (impact >= 0 ? "+" : ""), impact, (impact == 1 || impact == -1 ? " " : "s"));
     }
-    printf("\n=========================================================================================\n");
+    printf("\n-----------------------------------------------------------------------------------------\n");
 }
 
-static Metrics run_policy(int policy, unsigned int *sequence, int n) {
-    cache *c = cache_init(L1_NUM_SETS, L1_WAYS, L1_BLOCK_SIZE, policy);
-    for (int i = 0; i < n; i++) cache_access(c, sequence[i]);
+
+// Benchmark 1
+void simulate_streaming(cache *c) {
+    unsigned int base_array = 0x10000; 
+    unsigned int base_hot   = 0x20000; 
+    
+    // PCs fakes
+    unsigned int pc_array = 0x00400000; // Instrução que lê o array
+    unsigned int pc_hot   = 0x00400004; // Instrução que lê o dado importante
+    
+    for (int it = 0; it < 10; it++) {
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            // Acessa o dado passando o PC daquela instrução
+            cache_access(c, base_array + (i * 4), pc_array);
+            
+            if (i % 64 == 0) {
+                // Acessa o hot_data passando o PC dele
+                cache_access(c, base_hot, pc_hot);
+            }
+        }
+    }
+}
+
+// Benchmark 2
+void simulate_matrix_conv(cache *c) {
+    unsigned int base_img = 0x30000;
+    unsigned int base_out = 0x80000;
+    int width = 128;
+    int height = ARRAY_SIZE / width;
+    
+    unsigned int pc_img = 0x00400008; // Instrução que lê a imagem
+    unsigned int pc_out = 0x0040000C; // Instrução que escreve a saída
+    
+    for (int y = 1; y < height - 1; y++) {
+        for (int x = 1; x < width - 1; x++) {
+            cache_access(c, base_img + (((y-1)*width+x) * 4), pc_img);
+            cache_access(c, base_img + ((y*width+x) * 4), pc_img);
+            cache_access(c, base_img + (((y+1)*width+x) * 4), pc_img);
+            cache_access(c, base_out + ((y*width+x) * 4), pc_out);
+        }
+    }
+}
+
+void print_menu() {
+    printf("1. Streaming + HotSet (Antagonista ao LRU)\n");
+    printf("2. Matrix Convolution (Reuso Temporal)\n");
+    printf("0. Sair\n");
+    printf("Escolha uma opção: ");
+}
+
+
+static Metrics run_policy(int policy, int benchmark_id, int cap, int ways, int b_size) {
+    int num_sets = cap / (b_size * ways);
+    cache *c = cache_init(num_sets, ways, b_size, policy);
+    //for (int i = 0; i < n; i++) cache_access(c, sequence[i]);
+    
+    if (benchmark_id == 1) simulate_streaming(c);
+    else if (benchmark_id == 2) simulate_matrix_conv(c);
 
     int total = c->hit_count + c->miss_count;
     Metrics m;
@@ -129,61 +191,36 @@ void generate_sequence(unsigned int *sequence, int num_accesses, int type_acess)
     }
 }
 
-int main()
-{
-    printf("===========================================\n");
-    printf("  Simulador de Cache — Baseline LRU\n");
-    printf("  L1: %d conjuntos | %d vias | bloco %d bytes\n",
-           L1_NUM_SETS, L1_WAYS, L1_BLOCK_SIZE);
-    printf("===========================================\n");
-
-    int pattern;
-    printf("Escolha o padrão de acesso:\n");
-    printf("  1) Loop\n  2) Scan\n  3) Aleatório\n");
-    printf("Digite 1, 2 ou 3: ");
-    scanf("%d", &pattern);
-    if (pattern < 1 || pattern > 3) {
-        printf("Entrada inválida. Usando padrão aleatório.\n");
-        pattern = 3;
+int main(){
+    int choice = 1;
+    
+    while (choice != 0) {
+        print_menu();
+        if (scanf("%d", &choice) != 1) break;
+        
+        if (choice == 1 || choice == 2) {
+            
+            // 1. Roda o Baseline
+            Metrics lru = run_policy(0, choice, L1_CAPACITY, L1_WAYS, L1_BLOCK_SIZE);
+            PolicyResult results[2];
+            results[0] = (PolicyResult){ "Baseline (LRU)", lru };
+            int count = 1;
+            
+            // 2. Roda o Perceptron (AIRA1)
+            Metrics aira1 = run_policy(1, choice, L1_CAPACITY, L1_WAYS, L1_BLOCK_SIZE);
+            results[1] = (PolicyResult){ "AIRA1", aira1 };
+            count = 2;
+            
+            /*Metrics aira2 = run_policy(2, choice, L1_CAPACITY, L1_WAYS, L1_BLOCK_SIZE);
+            results[2] = (PolicyResult){ "AIRA2", aira2 };
+            count = 3;*/
+        
+            char nome_bench[50];
+            sprintf(nome_bench, "BENCHMARK %d", choice);
+            print_benchmark_table_flexible(nome_bench, "L1 cache simulada", results, count);
+        }
     }
-
-    unsigned int *sequence = (unsigned int *) malloc(NUM_ACCESSES * sizeof(unsigned int));
-    if (sequence == NULL) {
-        fprintf(stderr, "Erro ao alocar sequência.\n");
-        return 1;
-    }
-
-    generate_sequence(sequence, NUM_ACCESSES, pattern);
-
-
-
-    // 1. Roda sempre o Baseline (LRU)
-    Metrics lru_metrics = run_policy(0, sequence, NUM_ACCESSES);
     
-    // 2. Controla quantas políticas queremos comparar na tabela
-    int num_policies_to_compare = 1; 
-    PolicyResult results[3]; // Suporta até 3 colunas: LRU, AIRA1, AIRA2
-    
-    // Insere o Baseline na primeira posição
-    results[0] = (PolicyResult){ "Baseline (LRU)", lru_metrics };
-    
-    /*
-    Metrics aira1_metrics = run_policy(1, sequence, NUM_ACCESSES);
-    results[1] = (PolicyResult){ "AIRA1", aira1_metrics };
-    num_policies_to_compare = 2; 
-    */
-    
-    /*
-    Metrics aira2_metrics = run_policy(2, sequence, NUM_ACCESSES);
-    results[2] = (bPolicyResult){ "AIRA2", aira2_metrics };
-    num_policies_to_compare = 3;
-    */
-
-    // imprime a tabela de acordo com a quantidade de políticas ativadas
-    print_benchmark_table_flexible("BENCHMARK A", "L1 4KB e 2 vias, L2 32KB e 8 vias", results, num_policies_to_compare);
-
-
-
-    free(sequence);
+    printf("Encerrando...\n");
     return 0;
 }
