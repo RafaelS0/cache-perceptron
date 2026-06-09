ed60c81fb6b7e0b5a31de9525f4d28b61b47db9f
Deri <116681280+DerickMuniz@users.noreply.github.com>
2026-06-09 15:37:07 -0300

    Add new simulation functions for cache benchmarks

--- software/benchmark_c/benchmarks.h
+++ software/benchmark_c/benchmarks.h
@@ -6,6 +6,8 @@
 
 void simulate_streaming(cache *c);
 void simulate_matrix_conv(cache *c);
+void simulate_zigzag_access(cache *c);
+void simulate_hash_table_noise(cache *c);
 void simulate_validation_lru(cache *c);
 void simulate_validation_perceptron(cache *c);
 #endif
